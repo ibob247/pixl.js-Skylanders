724d50eabf83fed8b1ab3191b66ed9e65e51cd65
ibob247 <30962100+ibob247@users.noreply.github.com>
2026-06-18 20:13:24 -0400

    Comment out resource loading in event handling
    Comment out calls to tag_helper_load_coll_res_from_block0_with_switch_mode in previous and next event handling.

--- fw/application/src/app/chameleon/scene/chameleon_scene_main.c
+++ fw/application/src/app/chameleon/scene/chameleon_scene_main.c
@@ -19,12 +19,12 @@ static void chameleon_scene_main_event_cb(chameleon_view_event_t event, chameleo
         uint8_t slot = tag_emulation_get_slot();
         slot = tag_emulation_slot_find_prev(slot);
         tag_emulation_change_slot(slot, true);
-        tag_helper_load_coll_res_from_block0_with_switch_mode();
+        // tag_helper_load_coll_res_from_block0_with_switch_mode();
     } else if (event == CHAMELEON_VIEW_EVENT_NEXT) {
         uint8_t slot = tag_emulation_get_slot();
         slot = tag_emulation_slot_find_next(slot);
         tag_emulation_change_slot(slot, true);
-        tag_helper_load_coll_res_from_block0_with_switch_mode();
+        // tag_helper_load_coll_res_from_block0_with_switch_mode();
     }
 }
 
